982e58d1f950382f9da40d9ac12365992e999c33
Siddhartha Satyakama <52314451+mrsidrdx@users.noreply.github.com>
2020-01-05 17:44:20 +0530

    Update main.js

--- connect-four/main.js
+++ connect-four/main.js
@@ -111,7 +111,7 @@ function gameEnd(winningPlayer) {
       game_on = false;
     }
   }
-  var r= $('<br><a href="index.html"><button type="button" class="btn btn-primary btn-lg">Play Again!</button></a>');
+  var r= $('<br><a href=""><button type="button" class="btn btn-primary btn-lg">Play Again!</button></a>');
   $("#refresh").append(r);
 }
 
