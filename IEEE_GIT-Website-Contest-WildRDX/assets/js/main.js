9005e14bce6f4b8cf35d83627b79ff374046a3a4
Siddhartha Satyakama <52314451+mrsidrdx@users.noreply.github.com>
2020-08-12 14:00:00 +0530

    ieee-git project uploaded

---
+++ IEEE_GIT-Website-Contest-WildRDX/assets/js/main.js
@@ -0,0 +1,37 @@
+var btn = $('#backTop');
+
+$(window).on("load",function(){
+    setTimeout(function() {
+      $('.loader-wrapper').css({display: 'none'});
+      // $(".loader-wrapper").fadeOut("slow");
+      $('html, body').css({overflow: 'auto'});
+    }, 2400);
+});
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    console.log('yes');
+    $('#backTop').addClass('show');
+  } else {
+    $('#backTop').removeClass('show');
+    console.log('no');
+  }
+});
+
+$(document).ready(function(){
+  $("#backTop").click(function(e){
+    console.log('clicked');
+    e.preventDefault();
+    $('html, body').animate({scrollTop:0}, '300');
+  });
+});
+
+$(window).scroll(function () {
+	$(".slideanim").each(function () {
+		var pos = $(this).offset().top;
+		var winTop = $(window).scrollTop();
+		if (pos < winTop + 600) {
+			$(this).addClass("slide");
+		}
+	});
+});
